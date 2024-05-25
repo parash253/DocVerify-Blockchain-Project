28209d67153cd54e93de4a25bc950467c93c211c
parash253 <58897920+parash253@users.noreply.github.com>
2024-05-26 02:53:37 +0545

    This is certificate evaluation Dapp

--- truffle/contracts/SimpleStorage.sol
+++ truffle/contracts/SimpleStorage.sol
@@ -2,18 +2,115 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract SimpleStorage {
-    mapping(uint => string) public files;
-    uint public fileCount = 0;
+    struct File {
+        uint id;
+        string ipfsHash;
+    }
 
-    event FileAdded(uint fileId, string ipfsHash);
+    mapping(address => File[]) private userFiles;
+    mapping(string => bool) private fileExists;
+    mapping(address => mapping(string => bool)) private userFileExists;
+    mapping(address => uint) public userFileCount;
+
+    event FileAdded(address indexed user, uint fileId, string ipfsHash);
 
     function addFile(string memory _ipfsHash) public {
-        fileCount++;
-        files[fileCount] = _ipfsHash;
-        emit FileAdded(fileCount, _ipfsHash);
+        require(!fileExists[_ipfsHash], "File already exists in the blockchain.");
+        require(!userFileExists[msg.sender][_ipfsHash], "User already added this file.");
+        userFileCount[msg.sender]++;
+        userFiles[msg.sender].push(File(userFileCount[msg.sender], _ipfsHash));
+        fileExists[_ipfsHash] = true;
+        userFileExists[msg.sender][_ipfsHash] = true;
+        emit FileAdded(msg.sender, userFileCount[msg.sender], _ipfsHash);
     }
 
-    function getFile(uint _fileId) public view returns (string memory) {
-        return files[_fileId];
+    function getFile(address _user, uint _fileId) public view returns (string memory) {
+        require(_fileId > 0 && _fileId <= userFileCount[_user], "File ID out of range.");
+        return userFiles[_user][_fileId - 1].ipfsHash;
+    }
+
+    function getUserFiles(address _user) public view returns (File[] memory) {
+        return userFiles[_user];
+    }
+
+    function fileExistsInBlockchain(string memory _ipfsHash) public view returns (bool) {
+        return fileExists[_ipfsHash];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pragma solidity >=0.4.22 <0.9.0;
+
+// contract SimpleStorage {
+//     mapping(uint => string) public files;
+//     mapping(string => bool) private fileExists;
+//     uint public fileCount = 0;
+
+//     event FileAdded(uint fileId, string ipfsHash);
+
+//     function addFile(string memory _ipfsHash) public {
+//         require(!fileExists[_ipfsHash], "File already exists in the blockchain.");
+//         fileCount++;
+//         files[fileCount] = _ipfsHash;
+//         fileExists[_ipfsHash] = true;
+//         emit FileAdded(fileCount, _ipfsHash);
+//     }
+
+//     function getFile(uint _fileId) public view returns (string memory) {
+//         return files[_fileId];
+//     }
+
+//     function fileExistsInBlockchain(string memory _ipfsHash) public view returns (bool) {
+//         return fileExists[_ipfsHash];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// pragma solidity >=0.4.22 <0.9.0;
+
+// contract SimpleStorage {
+//     mapping(uint => string) public files;
+//     uint public fileCount = 0;
+
+//     event FileAdded(uint fileId, string ipfsHash);
+
+//     function addFile(string memory _ipfsHash) public {
+//         fileCount++;
+//         files[fileCount] = _ipfsHash;
+//         emit FileAdded(fileCount, _ipfsHash);
+//     }
+
+//     function getFile(uint _fileId) public view returns (string memory) {
+//         return files[_fileId];
+//     }
+// }
